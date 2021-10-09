51e3ca60ee351c62142b4af49dff2c0abd852016
J MN <jomarno530.git@gmail.com>
2021-10-09 13:57:18 +0200

    Added code in the 'Querrying the Database' section

--- cddb.lisp
+++ cddb.lisp
@@ -29,8 +29,7 @@
     (make-cd
         (prompt-read "Title")
         (prompt-read "Artist")
-        (or (parse-integer (prompt-read "Rating") 
-            :junk-allowed t) 0)
+        (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
         (y-or-n-p "Ripped [y/n]")
     )
 )
@@ -62,3 +61,23 @@
         )
     )
 )
+
+(defun select (selector-fn)
+    (remove-if-not selector-fn *db*)
+)
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd) 
+        (and
+            (if title (equal (getf cd :title) title) t)
+            (if artist (equal (getf cd :artist) artist) t)
+            (if rating (equal (getf cd :rating) rating) t)
+            (if ripped-p (equal (getf cd :ripped) ripped) t)
+        )
+    )
+)
+
+
+
+
+
